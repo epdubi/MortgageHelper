27033cfbba49719c197053b20cd2b9633504b943
ewagner <ericwagnerpreston@gmail.com>
2020-09-27 09:04:11 -0500

    Initial commit

---
+++ MonthlyPayment.cs
@@ -0,0 +1,16 @@
+class MonthlyPayment
+{
+    public int MonthNumber { get; set; }
+
+    public double StartingBalance { get; set; }
+
+    public double Payment { get; set; }
+
+    public double InterestPaid { get; set; }
+
+    public double PrincipalPaid { get; set; }
+
+    public double TaxesAndInsurance { get; set; }
+
+    public double EndingBalance { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using CsvHelper;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace MortgageHelper
+{
+    class Program
+    {
+        private static double TotalPrincipal => 0;
+
+        private static double InterestRate => .0;
+
+        private static double MonthlyPayment => 0;
+
+        private static double MonthlyTaxesAndInsureance => 0;
+
+        private static IEnumerable<int> LoanLengthInMonths => Enumerable.Range(1, 60);
+
+        private static double MonthInterest(double outstandingPrincipal, double mortgageRate) => outstandingPrincipal * mortgageRate / 12;
+
+        private static double MonthPrincipal(double monthlyPaidInterest) => MonthlyPayment - monthlyPaidInterest;
+
+        static void Main(string[] args)
+        {
+            var runningPrincipal = TotalPrincipal;
+            var payments = new List<MonthlyPayment>();
+
+            foreach (var month in LoanLengthInMonths)
+            {
+                var monthlyPaidInterest = MonthInterest(runningPrincipal, InterestRate);
+                var monthlyPaidPrincipal = MonthPrincipal(monthlyPaidInterest) - MonthlyTaxesAndInsureance;
+                var monthlyEndingBalance = runningPrincipal - monthlyPaidPrincipal;
+
+                payments.Add(new MonthlyPayment { MonthNumber = month, StartingBalance = runningPrincipal, Payment = MonthlyPayment, InterestPaid = monthlyPaidInterest, PrincipalPaid = monthlyPaidPrincipal, TaxesAndInsurance = MonthlyTaxesAndInsureance, EndingBalance = monthlyEndingBalance });
+                runningPrincipal = monthlyEndingBalance;
+            }
+
+            var currentDir = Directory.GetCurrentDirectory();
+            using (var writer = new StreamWriter(@$"{currentDir}\output.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(payments);
+            }
+        }
+    }
+}
